49dfb622a68f0517c213d3997aac7e0a3cf61195
Semyon Pukhov <semyonpuchof@gmail.com>
2021-04-12 13:01:48 +0300

    Fix RestTool component

--- src/RestTool/index.tsx
+++ src/RestTool/index.tsx
@@ -1,146 +1,135 @@
-import React, { useEffect, useState } from "react"
-import _ from "the-lodash"
+import React, { useEffect, useState } from 'react';
+import _ from 'the-lodash';
 
-import { EndpointInfo, ExpandedUserService, HttpParam } from "./types"
-import { HttpMethod } from "@kubevious/ui-framework"
+import { EndpointInfo, ExpandedUserService, HttpParam } from './types';
+import { HttpMethod } from '@kubevious/ui-framework';
 
-import "./styles.scss"
+import './styles.scss';
 
-import { useService } from "@kubevious/ui-framework"
-import { sharedState } from "@kubevious/ui-framework/dist/global"
-import { YamlControlBar, CopyButton } from "@kubevious/ui-components/dist"
+import { useService } from '@kubevious/ui-framework';
+import { sharedState } from '@kubevious/ui-framework/dist/global';
+import { YamlControlBar, CopyButton } from '@kubevious/ui-components/dist';
 
 const isTesting = process.env.IS_TESTING;
 
-
 export const RestTool = () => {
-    const service = useService<ExpandedUserService>({ kind: "user" })
+    const service = !isTesting ? useService<ExpandedUserService>({ kind: 'user' }) : null;
 
-    const [editedRequestData, setEditedRequestData] = useState("{}")
-    const [requestMethod, setRequestMethod] = useState(
-        HttpMethod.GET.toString()
-    )
-    const [requestPath, setRequestPath] = useState("")
-    const [endpoints, setEndpoints] = useState<EndpointInfo[]>([])
-    const [requestParams, setRequestParams] = useState<HttpParam[]>([])
-    const [paramsHistory, setParamsHistory] = useState<Record<string, string>>(
-        {}
-    )
+    const [editedRequestData, setEditedRequestData] = useState('{}');
+    const [requestMethod, setRequestMethod] = useState(HttpMethod.GET.toString());
+    const [requestPath, setRequestPath] = useState('');
+    const [endpoints, setEndpoints] = useState<EndpointInfo[]>([]);
+    const [requestParams, setRequestParams] = useState<HttpParam[]>([]);
+    const [paramsHistory, setParamsHistory] = useState<Record<string, string>>({});
 
-    const [responseData, setResponseData] = useState("Response")
-    const [responseCode, setResponseCode] = useState("")
+    const [responseData, setResponseData] = useState('Response');
+    const [responseCode, setResponseCode] = useState('');
 
-    const [userData, setUserData] = useState("")
-    const [accessToken, setAccessToken] = useState("")
+    const [userData, setUserData] = useState('');
+    const [accessToken, setAccessToken] = useState('');
 
     useEffect(() => {
-        const endpointsFromState = sharedState.get('endpoints')
+        setTimeout(() => {
+            const endpointsFromState = sharedState.get('endpoints');
 
-        const generatedEndpoints: (string | EndpointInfo[]) = _.map(endpointsFromState, (x) => {
-            if (_.isString(x)) {
-                return {
-                    name: x,
-                    method: HttpMethod.GET,
+            const generatedEndpoints: string | EndpointInfo[] = _.map(endpointsFromState, (x) => {
+                if (_.isString(x)) {
+                    return {
+                        name: x,
+                        method: HttpMethod.GET,
+                    };
+                } else {
+                    return x;
                 }
-            } else {
-                return x
-            }
-        })
+            });
 
-        setEndpoints(generatedEndpoints)
-    }, [])
-
+            setEndpoints(generatedEndpoints);
+        }, 100);
+    }, []);
 
     const endpointsData: Record<string, EndpointInfo> = _.makeDict(
         endpoints,
         (x) => {
-            return `${x.method} :: ${x.name}`
+            return `${x.method} :: ${x.name}`;
         },
-        (x) => x
-    )
+        (x) => x,
+    );
 
     useEffect(() => {
-        setAccessToken(service?.accessToken() || '')
-        setUserData(service?.userData() || '')
-    }, [service])
+        setAccessToken(service?.accessToken() || '');
+        setUserData(service?.userData() || '');
+    }, [service]);
 
     const handleChangeRequest = ({ value }: { value: string }): void => {
-        setEditedRequestData(value)
-    }
+        setEditedRequestData(value);
+    };
 
     const handleChangeAccessToken = ({ value }: { value: string }): void => {
-        setUserData(value)
-    }
+        setUserData(value);
+    };
 
     const handleChangeResponse = ({ value }: { value: string }): void => {
-        setResponseData(value)
-    }
+        setResponseData(value);
+    };
 
     const handleChangePath = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setRequestPath(e.target.value)
-    }
+        setRequestPath(e.target.value);
+    };
 
-    const handleChangeRequestType = (
-        e: React.ChangeEvent<HTMLSelectElement>
-    ): void => {
-        setRequestMethod(e.target.value)
-    }
+    const handleChangeRequestType = (e: React.ChangeEvent<HTMLSelectElement>): void => {
+        setRequestMethod(e.target.value);
+    };
 
     const handleSendRequest = async (): Promise<void> => {
-        let params: Record<string, string> | undefined = undefined
-        let data: Record<string, any> | undefined = undefined
+        let params: Record<string, string> | undefined = undefined;
+        let data: Record<string, any> | undefined = undefined;
 
         params = _.makeDict(
             requestParams,
             (x) => x.name,
-            (x) => x.value
-        )
+            (x) => x.value,
+        );
 
         if (requestMethod !== HttpMethod.GET) {
             if (editedRequestData) {
-                data = JSON.parse(editedRequestData)
+                data = JSON.parse(editedRequestData);
             } else {
-                data = {}
+                data = {};
             }
         }
 
-        const response = await service?.client.execute(
-            requestMethod as HttpMethod,
-            requestPath,
-            params,
-            data
-        )
+        const response = await service?.client.execute(requestMethod as HttpMethod, requestPath, params, data);
         if (response) {
-            setResponseData(JSON.stringify(response.data, null, 2))
+            setResponseData(JSON.stringify(response.data, null, 2));
             if (response.status) {
-                setResponseCode(response.status.toString())
+                setResponseCode(response.status.toString());
             }
         }
-    }
+    };
 
     const renderEndpointsTemplateSelector = () => {
         return (
             <select
                 className="select"
                 onChange={(e) => {
-                    const selectedTemplate = endpointsData[e.target.value]
-                    setRequestPath(selectedTemplate.name)
-                    setRequestMethod(selectedTemplate.method.toString())
+                    const selectedTemplate = endpointsData[e.target.value];
+                    setRequestPath(selectedTemplate.name);
+                    setRequestMethod(selectedTemplate.method.toString());
                     if (selectedTemplate.params) {
                         setRequestParams(
                             selectedTemplate.params.map((x) => {
                                 return {
                                     name: x,
-                                    value: paramsHistory[x] || "",
-                                }
-                            })
-                        )
+                                    value: paramsHistory[x] || '',
+                                };
+                            }),
+                        );
                     } else {
-                        setRequestParams([])
+                        setRequestParams([]);
                     }
                     if (selectedTemplate.sample) {
                         if (selectedTemplate.sample.body) {
-                            setEditedRequestData(JSON.stringify(selectedTemplate.sample.body, null, 4))
+                            setEditedRequestData(JSON.stringify(selectedTemplate.sample.body, null, 4));
                         }
                     }
                 }}
@@ -152,8 +141,8 @@ export const RestTool = () => {
                     </option>
                 ))}
             </select>
-        )
-    }
+        );
+    };
 
     return (
         <>
@@ -168,11 +157,7 @@ export const RestTool = () => {
 
                     <div>
                         <label className="label">Method: </label>
-                        <select
-                            className="select"
-                            onChange={(e) => handleChangeRequestType(e)}
-                            value={requestMethod}
-                        >
+                        <select className="select" onChange={(e) => handleChangeRequestType(e)} value={requestMethod}>
                             {_.keys(HttpMethod).map((method, index) => (
                                 <option key={index} value={method.toString()}>
                                     {method}
@@ -198,16 +183,11 @@ export const RestTool = () => {
                                         value={param.name}
                                         className="path-input input"
                                         onChange={(e) => {
-                                            param.name = e.target.value
-                                            setRequestParams(
-                                                _.clone(requestParams)
-                                            )
+                                            param.name = e.target.value;
+                                            setRequestParams(_.clone(requestParams));
 
-                                            paramsHistory[param.name] =
-                                                param.value
-                                            setParamsHistory(
-                                                _.clone(paramsHistory)
-                                            )
+                                            paramsHistory[param.name] = param.value;
+                                            setParamsHistory(_.clone(paramsHistory));
                                         }}
                                     />
                                     <input
@@ -215,30 +195,21 @@ export const RestTool = () => {
                                         value={param.value}
                                         className="path-input input"
                                         onChange={(e) => {
-                                            param.value = e.target.value
-                                            setRequestParams(
-                                                _.clone(requestParams)
-                                            )
+                                            param.value = e.target.value;
+                                            setRequestParams(_.clone(requestParams));
 
-                                            paramsHistory[param.name] =
-                                                param.value
-                                            setParamsHistory(
-                                                _.clone(paramsHistory)
-                                            )
+                                            paramsHistory[param.name] = param.value;
+                                            setParamsHistory(_.clone(paramsHistory));
                                         }}
                                     />
                                     <button
                                         className="main-btn"
                                         onClick={() => {
-                                            requestParams.splice(index, 1)
-                                            setRequestParams(
-                                                _.clone(requestParams)
-                                            )
+                                            requestParams.splice(index, 1);
+                                            setRequestParams(_.clone(requestParams));
 
-                                            delete paramsHistory[param.name]
-                                            setParamsHistory(
-                                                _.clone(paramsHistory)
-                                            )
+                                            delete paramsHistory[param.name];
+                                            setParamsHistory(_.clone(paramsHistory));
                                         }}
                                     >
                                         Delete
@@ -249,73 +220,69 @@ export const RestTool = () => {
                                 className="main-btn"
                                 onClick={() => {
                                     requestParams.push({
-                                        name: "",
-                                        value: "",
-                                    })
-                                    setRequestParams(_.clone(requestParams))
+                                        name: '',
+                                        value: '',
+                                    });
+                                    setRequestParams(_.clone(requestParams));
                                 }}
                             >
                                 Add
                             </button>
                         </>
                     )}
-                    {!isTesting && <div className="text-area-container">
-                        {requestMethod !== HttpMethod.GET && (
-                            <>
-                                <div className="text-area-label">
-                                    Request Data:{" "}
-                                </div>
-                                <YamlControlBar
-                                    value={editedRequestData}
-                                    beforeChange={handleChangeRequest}
-                                    text={editedRequestData}
-                                    downloadButton
-                                />
-                            </>
-                        )}
-                        <div className="btn-wrapper">
-                            <button
-                                className="main-btn send"
-                                onClick={() => handleSendRequest()}
-                            >
-                                SEND
-                            </button>
+                    {!isTesting && (
+                        <div className="text-area-container">
+                            {requestMethod !== HttpMethod.GET && (
+                                <>
+                                    <div className="text-area-label">Request Data: </div>
+                                    <YamlControlBar
+                                        value={editedRequestData}
+                                        beforeChange={handleChangeRequest}
+                                        text={editedRequestData}
+                                        downloadButton
+                                    />
+                                </>
+                            )}
+                            <div className="btn-wrapper">
+                                <button className="main-btn send" onClick={() => handleSendRequest()}>
+                                    SEND
+                                </button>
+                            </div>
                         </div>
-                    </div>}
+                    )}
                 </div>
                 <div className="section">
                     <h3>Response</h3>
-                    {!isTesting && <div className="text-area-container">
-                        <div className="text-area-label">
-                            Status Code: {responseCode}
+                    {!isTesting && (
+                        <div className="text-area-container">
+                            <div className="text-area-label">Status Code: {responseCode}</div>
+                            <YamlControlBar
+                                value={responseData}
+                                beforeChange={handleChangeResponse}
+                                text={responseData}
+                                downloadButton
+                            />
                         </div>
-                        <YamlControlBar
-                            value={responseData}
-                            beforeChange={handleChangeResponse}
-                            text={responseData}
-                            downloadButton
-                        />
-                    </div>}
+                    )}
                 </div>
                 <div className="section">
                     <h3>Auth Info</h3>
-                    {!isTesting && <div className="text-area-container">
-                        <div className="text-area-label">Info about user:</div>
-                        <YamlControlBar
-                            value={userData}
-                            beforeChange={handleChangeAccessToken}
-                            text={userData}
-                            downloadButton
-                        />
-                        <div className="btn-wrapper">
-                            <CopyButton
-                                text={accessToken}
-                                buttonText="COPY ACCESS TOKEN"
+                    {!isTesting && (
+                        <div className="text-area-container">
+                            <div className="text-area-label">Info about user:</div>
+                            <YamlControlBar
+                                value={userData}
+                                beforeChange={handleChangeAccessToken}
+                                text={userData}
+                                downloadButton
                             />
+                            <div className="btn-wrapper">
+                                <CopyButton text={accessToken} buttonText="COPY ACCESS TOKEN" />
+                            </div>
                         </div>
-                    </div>}
+                    )}
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
--- test/restTool.test.tsx
+++ test/restTool.test.tsx
@@ -1,18 +1,18 @@
-// import React from 'react';
+import React from 'react';
 import 'jest'
 
-// import { RestTool } from "../src";
-// import { render } from "@testing-library/react";
+import { RestTool } from "../src";
+import { render } from "@testing-library/react";
 
-// function renderRestTool() {
-  // return render(<RestTool />);
-// }
+function renderRestTool() {
+  return render(<RestTool />);
+}
 
 describe("<RestTool />", () => {
   test("Should check that the component RestTool is rendered", async () => {
-    // const { findByTestId } = renderRestTool();
+    const { findByTestId } = renderRestTool();
 
-    // const restTool = await findByTestId("rest-tool");
-    // expect(restTool)
+    const restTool = await findByTestId("rest-tool");
+    expect(restTool)
   });
 });
